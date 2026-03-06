cbe4542a134a7bfda5102a0a767681e340fa5a35
Guilherme Miguel <guilherme12miguel123@gmail.com>
2026-03-05 23:39:21 -0300

    Corrigir o caminho base do GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // O SEGREDO ESTÁ AQUI: O nome tem que ser EXATAMENTE igual ao link do GitHub
+  base: '/Cart-oaniversario/', 
 })
